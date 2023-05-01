7c22140d6d38b45f4e0d73c8386162d15f9dbb37
jerrysam <jeromeminney@gmail.com>
2023-05-01 20:28:09 +0100

    now plotting average, UQ, future_shifted (where consumption grows ~80% and peak is preserved), and future_worst (where peak increases ~80% requiring transformer upgrades)

--- plot.py
+++ plot.py
@@ -29,15 +29,8 @@ for unit in unique_units:
 
     # Remove columns that contain missing or invalid values
     unit_data = unit_data.dropna(axis='columns', how='any')
-    
-    # Convert the "Value" column to numeric data type
-    unit_data = unit_data.apply(pd.to_numeric, errors='coerce')
-    
-    unit_data = unit_data.select_dtypes(include=[np.number])
 
-    if len(unit_data.columns) == 0:
-        print(f"No numeric data found for unit {unit}")
-        continue
+    unit_data = unit_data.select_dtypes(include=[np.number])
 
     # Select data for the first week of February
     start_date = pd.Timestamp('2023-02-01').tz_localize(unit_data.index.tz)
@@ -47,19 +40,42 @@ for unit in unique_units:
     # Calculate the upper quartile for each line
     upper_quartiles = unit_data.quantile(0.75, axis=0)
 
-    # Plot the original values
-    fig, ax = plt.subplots()
-    unit_data.plot(ax=ax, linewidth=0.25, legend=True)
+    # Find the column with the highest upper quartile value
+    max_quartile_column = upper_quartiles.idxmax()
 
-    # Plot the upper quartile as a flat line across the chart with the same color as the time series
-    for i, column in enumerate(upper_quartiles.index):
-        upper_quartile = upper_quartiles[column]
-        color = colors[i % len(colors)]
-        ax.axhline(upper_quartile, color=color, linestyle='--', linewidth=1.5)
-
-    ax.set_ylabel('Value')
-    ax.set_title(f'Unit: {unit}')
-
-    # Show the plot
+    # Plot the original values only for unit == "kW"
     if unit == "kW":
+        original_values = unit_data[max_quartile_column]
+        upper_quartile = upper_quartiles[max_quartile_column]
+
+        # Calculate the total values by summing the original and second set of values
+        future_values = np.minimum((3*original_values), 0.9*original_values.max())
+
+        # Calculate the average of the total values
+        average_future = future_values.mean()
+        # Calculate the average of the original values
+        average_original = original_values.mean()
+
+        #Calculate the worst case (upgrade transformers) scenario
+        growth_multiplier = average_future / average_original
+        worst_case = original_values * growth_multiplier
+
+        # Plot the original values, second set of values, and the sum of both
+        fig, ax = plt.subplots()
+        original_values.plot(ax=ax, label='Original kW', linewidth=1)
+        future_values.plot(ax=ax, label='Total kW - Shift loads', color='green', linewidth=0.5)
+        worst_case.plot(ax=ax, label='Total kW - Upgrade transformers', color='#FF7F0E', linewidth=0.25)
+        # Fill the area under the graph between the second set and total values
+        ax.fill_between(original_values.index, future_values, color='gray', alpha=0.2)
+        ax.fill_between(original_values.index, original_values, color='gray', alpha=0.4)
+
+        ax.axhline(upper_quartile, linestyle='--', color='#1F77B4', linewidth=0.5, label='UpperQuartile Original')
+        ax.axhline(average_original, linestyle='--', color='#1F77B4', linewidth=0.5, label='Average Original')
+        ax.axhline(average_future, linestyle='--', color='green', linewidth=0.5, label='Average Total')
+        
+
+        ax.set_ylabel('Value')
+        ax.set_title(f'Unit: {unit}')
+        ax.legend()
+
         plt.show()
